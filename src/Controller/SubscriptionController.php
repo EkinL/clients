3dcc511844eda50aad6cd33c4b3e92a89f01b14c
Lilian Hammache <fayzerdev@gmail.com>
2025-01-08 16:36:18 +0100

    update

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -11,22 +11,10 @@ class SubscriptionController extends AbstractController
     #[Route('/subscription', name: 'app_subscription')]
     public function index(): Response
     {
-        // Strip
-        $stripe = new \Stripe\StripeClient('sk_test_51PYFSpBTpbjFXff3MDqBk77DN1Pt4DuncdGrdfOUqnKEQOE4vrAbnBVZaZa9NxfYwjDdaRsk9Wu505Ocy1tq73lD001ZgRtLYt');
-        $stripe->subscriptions->create([
-            'customer' => 'cus_J4Zz1Zz1Zz1Zz1',
-            'items' => [
-                [
-                    'price' => 'price_1J4Zz1Zz1Zz1Zz1',
-                ],
-            ],
-        ]);
-
 
 
         return $this->render('subscription/index.html.twig', [
             'controller_name' => 'SubscriptionController',
-            'stripe' => $stripe,
         ]);
     }
 }
