9423f4cf6a274ae51c7e75375bb6d5391cdacb8c
Lilian Hammache <fayzerdev@gmail.com>
2025-01-10 09:54:46 +0100

    [FIX] Création du frontend de la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProjectsRepository;
+use App\Repository\ClientsRepository;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ProjectsRepository $projectsRepository, ClientsRepository $clientsRepository): Response
     {
+        $projects = $projectsRepository->findBy([], ['id' => 'DESC'], 4);
+
+        $clients = $clientsRepository->findBy([], ['id' => 'DESC'], 4);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'projects' => $projects,
+            'clients' => $clients
         ]);
     }
 }
